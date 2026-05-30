c4220f46d29595b082490599485cfd96e054ec22
Nataliya.Valtman <nataliya.valtman@jetbrains.com>
2026-05-30 11:14:34 +0200

    fix: add missing import for ExperimentalWasmDsl class

--- npm-publish-gradle-plugin/src/main/kotlin/config/kotlinJsTarget.kt
+++ npm-publish-gradle-plugin/src/main/kotlin/config/kotlinJsTarget.kt
@@ -19,6 +19,7 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Copy
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJsCompilation
 import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsBinaryMode
 import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsTargetDsl
